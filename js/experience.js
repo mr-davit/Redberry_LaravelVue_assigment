4e1e594eda3c151773048c04b68c6c90a7b0718b
mr-davit <77172061+mr-davit@users.noreply.github.com>
2022-07-10 08:45:52 +0400

    site 1.0 is deploied

--- js/experience.js
+++ js/experience.js
@@ -1,3 +1,23 @@
+const nextBtn = document.getElementById('next');
+const dropBtn = document.getElementById('dropBtn')
+const dropBtnGrand = document.getElementById('dropBtn_grand')
+
+//check if window is reloaded or closed, if reloaded keep values, if closed reset localstorage
+window.onload = (event) => {
+  if (window.sessionStorage.getItem('experience') == 1 && window.sessionStorage.getItem('grandmaster') == 1 ){
+    nextBtn.innerHTML = "Done";
+    document.getElementById('experience_anc').innerText = ' ';
+    document.getElementById('experience_anc').className = 'nav_anc anc_bg';
+    dropBtn.innerText = window.localStorage.getItem('experience_name');
+    dropBtnGrand.innerText = window.localStorage.getItem('master');
+  } 
+  // else {
+  //   window.localStorage.clear();
+  // }
+
+}
+
+
 // open the dropdown menu when user clicks on button
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
@@ -34,30 +54,36 @@ function myFunctionGrand() {
 
 
 //save user select for experience in local storage
-  function setExperience(id) {
-    console.log(id);
+  function setExperience(id,name) {
     window.localStorage.setItem('experience_level', id);
+    window.localStorage.setItem('experience_name', name);
     let dropBtn = document.getElementById("dropBtn");
     dropBtn.innerText = id;
     dropBtn.style.textTransform = "capitalize"
-    
-    console.log(window.localStorage.getItem('experience_level'));
+   
     document.getElementById('experience_anc').style.backgroundColor = '#E9FAF1';
+    
+    window.sessionStorage.setItem('experience' , '1');
 
+    checkComplete();
   }
 
 //save user select for Grandmaster in local storage
-  function getGrandmaster(id,master) {
+  function getGrandmaster(master,id) {
     console.log(id);
+    window.localStorage.setItem('master_id', id);
     window.localStorage.setItem('master', master);
-    let dropBtn = document.getElementById("dropBtn_grand");
-    dropBtn.innerText = id;
-    dropBtn.style.textTransform = "capitalize"
-    
-    console.log(window.localStorage.getItem('master'));
+    dropBtnGrand.innerText = window.localStorage.getItem('master');
+    dropBtnGrand.style.textTransform = "capitalize"
+    document.getElementById('experience_anc').style.backgroundColor = '#E9FAF1';
+
+
+    window.sessionStorage.setItem('grandmaster' , '1');
+    checkComplete();
+
   }
 
-  
+  // fetch GET data from API GRANDMASTER and Translate to html
   let array = [];  
   let a = [];
   fetch("https://chess-tournament-api.devtest.ge/api/grandmasters", {
@@ -79,28 +105,60 @@ function myFunctionGrand() {
         document.getElementById("myDropdownGrand").innerHTML += '<a id="' + element.id + '" class="GrandmasterA" onclick="getGrandmaster(this.innerText,this.id)" href="#">'+ element.name + '<img src="https://chess-tournament-api.devtest.ge' + element.image + '" alt="">'   +  '</a>';
       });
      
-     console.log(a);
   });
-    //   for (var i = 0; i < Object.keys(data).length; i++) {
-    //     let kote = (Object.values(data[i]));
-    //     console.log(data+"ეს არის დატას ლოგი");
-    //     for (var i = 0; i < Object.keys(kote).length; i++) {
-    //       console.log(kote+"ეს არის კოტეს ლოგო");
-    //       let a = '<a id="' + kote[0] + '" onclick="getGrandmaster(this.id)" href="#">'+ '<p>'+ kote[1] + '</p>' + '<img src="https://chess-tournament-api.devtest.ge' + kote[2] + '" alt="">  </a>';
-    //       console.log(a);
-    //       // document.getElementById("myDropdownGrand").innerText = a;
-    //       document.getElementById("myDropdownGrand").innerHTML = a;
-    //  }
-    //   }
-
-
-  
-
-
-      // for (var i = 0; i < Object.keys(kote).length; i++) {
-      //   console.log(kote[i]);
-      //   let a = '<a id="' + kote[0] + '" onclick="getGrandmaster(this.id)" href="#">'+ '<p>'+ kote[1] + '</p>' + '<img src="https://chess-tournament-api.devtest.ge' + kote[2] + '" alt="">  </a>';
-      //   console.log(a);
-      //   // document.getElementById("myDropdownGrand").innerText = a;
-      //   document.getElementById("myDropdownGrand").innerHTML = a;
    
+
+
+// check if all required data is endered and change button text from next to done 
+function checkComplete(){
+  if (window.sessionStorage.getItem('experience') == 1 && window.sessionStorage.getItem('grandmaster') == 1 ) {
+    nextBtn.innerHTML = "Done";
+    document.getElementById('experience_anc').innerText = ' ';
+    document.getElementById('experience_anc').className = 'nav_anc anc_bg';
+
+  }
+}
+
+//check if registration is complited, send data and redirect to congratulations page
+let participated = window.localStorage.getItem('participated');
+
+if (participated == 'true'){
+  participated = true;
+} else { participated = false;
+
+};
+
+
+function checkRegistration(){
+
+  if ( nextBtn.innerText == "Done"){
+    
+    fetch("https://chess-tournament-api.devtest.ge/api/register", {
+      method: 'POST',
+      body: JSON.stringify({
+        "name": window.localStorage.getItem('username') ,
+        "email": window.localStorage.getItem('email'),
+        "phone": window.localStorage.getItem('phone'),
+        "date_of_birth": window.localStorage.getItem('date_of_birth'),
+        "experience_level": window.localStorage.getItem('experience_level'),
+        "already_participated": participated,
+        "character_id": window.localStorage.getItem('master_id')
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(response=>{
+      return response
+    }).then(data=>{
+      if (data.status = 201){
+        window.location.replace("/done.html");
+        window.localStorage.clear();
+        window.sessionStorage.clear();
+      } else {
+        alert('something went wrong')
+      }
+    })
+  }
+
+};
--- js/registration.js
+++ js/registration.js
@@ -3,36 +3,87 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const date = document.getElementById('date');
+const errorWindow = document.getElementById('error');
+const input = document.querySelectorAll('input');
 
+let userLocal = '1';
+let emailLocal = '1';
+let phoneLocal = '1';
+let dateLocal = '1';
 
-// save input in local storage
-form.addEventListener('keyup', e =>
+let usernameVal  = "";
+let emailVal     = "";
+let phoneVal     = ""; 
+let dateVal      = "";
+
+let userOnInput = '';
+let emailOnInput = '';
+let phoneOnInput = '';
+let dateOnInput = '';
+let setSucess = '';
+
+let submited = "";
+
+// form.addEventListener('keypress',  e =>
+
+// {
+    
+//     checkInputsOnChange();
+// }
+
+// )
+
+form.addEventListener('input',  e =>
 {
-    const usernameVal  = username.value.trim();
-    const emailVal     = email.value.trim();
-    const phoneVal     = phone.value.trim(); 
-    const dateVal      = date.value.trim();
-    window.localStorage.setItem('username', usernameVal);
-    window.localStorage.setItem('email', emailVal);
-    window.localStorage.setItem('phone', phoneVal);
-    window.localStorage.setItem('date_of_birth', dateVal);
-    window.sessionStorage.setItem('1','1');
-    document.getElementById('personal_anc').style.backgroundColor = '#E9FAF1';
+
+    setLocal();
+    
+    checkInputsOnChange();
+    window.sessionStorage.setItem('wasPassed','false');
 
 }
 
 )
 
 
-const userLocal = window.localStorage.getItem('username');
-const emailLocal = window.localStorage.getItem('email');
-const phoneLocal = window.localStorage.getItem('phone');
-const dateLocal = window.localStorage.getItem('date_of_birth');
+
+
+
+//regex
+
+
+let called = 0;
+let notEmpty = 0;
+
+
+
+
+function setLocal (){
+    usernameVal  = username.value.trim();
+    emailVal     = email.value.trim();
+    phoneVal     = phone.value.trim(); 
+    dateVal      = date.value.trim();
+  
+    window.localStorage.setItem('username', usernameVal);
+    window.localStorage.setItem('email', emailVal);
+    window.localStorage.setItem('phone', phoneVal);
+    window.localStorage.setItem('date_of_birth', dateVal);
+    window.sessionStorage.setItem('reg_reload','1');
+    document.getElementById('personal_anc').style.backgroundColor = '#E9FAF1';
+
+}
+
+
+userLocal  = window.localStorage.getItem('username');
+emailLocal = window.localStorage.getItem('email');
+phoneLocal  = window.localStorage.getItem('phone');
+dateLocal  = window.localStorage.getItem('date_of_birth');
+
 
 
 //load content from local storage when reloading page
 window.onload = (event) => {
-    let checkClose = window.sessionStorage.getItem('1');
+    let checkClose = window.sessionStorage.getItem('reg_reload');
     console.log(checkClose);
     if (checkClose == 1 ) {
     document.getElementById('personal_anc').style.backgroundColor = '#E9FAF1';
@@ -40,8 +91,11 @@ window.onload = (event) => {
     email.value= emailLocal;
     phone.value=phoneLocal;
     date.value=dateLocal;
-    } else{
+    // document.getElementById('date_label').innerText=dateLocal;
+    
+    } else {
         window.localStorage.clear();
+        checkInputsOnChange();
     }
 };
 
@@ -49,45 +103,139 @@ window.onload = (event) => {
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
-	checkInputs();
+	checkInputsOnSubmit();
+    checkInputsOnChange();
+
+    // window.localStorage.setItem('username', usernameVal);
+    // window.localStorage.setItem('email', emailVal);
+    // window.localStorage.setItem('phone', phoneVal);
+    // window.localStorage.setItem('date_of_birth', dateVal);
+    // window.sessionStorage.setItem('reg_reload','1');
+    
+    // check if everything is correct, if yes change error window and navigation style & redirect to experience page
+    console.log(errorWindow.className == "none")
+ 
+// if (window.getElementById('error').className == "error_window"  ) {
+//     errorWindow.className = "error_window";
+// }
+//  else 
+//  if ( ( setinputname == true && setinputemail == true && setinputphone == true && setinputdate == true 
+
+ if ( (errorWindow.className == "none" && setSucessname == true && setSucessemail == true && setSucessphone == true && setSucessdate == true) || window.sessionStorage.getItem('wasPassed') == 'true') {
+        document.getElementById('personal_anc').innerText = ' ';
+        document.getElementById('personal_anc').className = 'nav_anc anc_bg';
+        window.sessionStorage.setItem('wasPassed','true');
+        window.location.replace("/experience.html");
+
+        console.log("mushaobs")
+    }
+
 });
 
-function checkInputs(){ 
- 
-    const usernameVal  = username.value.trim();
-    const emailVal     = email.value.trim();
-    const phoneVal     = phone.value.trim(); 
-    const dateVal      = date.value.trim();
-    let userReg =  /^[a-zA-Z]+$/.test(usernameVal);
-    let emailReg= /^[^\s@]+@redberry.ge$/.test(emailVal);
-    let phoneReg= /^[0-9]{9}$/.test(phoneVal);
-    let dateReg= /^\d{4}-\d{2}-\d{2}$/.test(dateVal);
-    const errorWindow = document.getElementById('error');
-    let called = 1  ;
 
 
+
+let setinputname = "";
+let setinputemail = "";
+let setinputphone = "";
+let setinputdate = "";
+
+
+
+function checkInputsOnChange(id){ 
+
+let userReg =  /^[a-zA-Z]+$/.test(usernameVal);
+let emailReg= /^[^\s@]+@redberry.ge$/.test(emailVal);
+let phoneReg= /^[0-9]{9}$/.test(phoneVal);
+let dateReg= /^\d{4}-\d{2}-\d{2}$/.test(dateVal);
+
+
+    if (id == 'username') {
     //username validation
-    if(usernameVal === '') {
-		setErrorFor(username,   'Invalid Username' ,'Username cannot be blank');
- 
-	} else if (userReg == false ){
+    console.log(userReg);
+     if (userReg == false ){
         setErrorFor(username, 'Invalid Username' ,'Username only can contain string');
+        setinputname = false;
     }
-    
+
     else {
 		setSuccessFor(username);
+        called += 1;
+        setinputname = true;
+	}
+}
+
+    if (id == 'email')
+    //email validation
+     if (emailReg == false ){
+        setErrorFor(email, 'Invalid email' ,'Please enter valid email address');
+        setinputemail = false;
+    }
+        else if (email == ""){
+        
+    }
+    else {
+		setSuccessFor(email);
+        called += 1;
+        setinputemail = true;
+	}
+
+
+
+    //phone number validation 
+    if (id == 'phone')
+     if (phoneReg == false ){
+        setErrorFor(phone, 'Invalid phone' ,'Please enter valid phone number ');
+        setinputphone = false;
+    }
+
+    else {
+		setSuccessFor(phone);
+        called += 1;
+        setinputphone =true;
+	}
+
+    if (id == 'date'){
+    //date validation
+     if (dateReg == false ){
+        setErrorFor(date, 'Invalid date' ,'Please enter valid date ');
+        setinputdate = false;
+    }
+    else if (dateVal == ""){
+        
+    }
+    else {
+        setSuccessFor(date);
+        called += 1;
+        setinputdate = true;
+    }
+    }
+}
+
+function checkInputsOnSubmit(){
+
+    if( usernameVal.length < 2  ) {
+		setErrorFor(username,   'Invalid Username' ,'Username cannot contain less then 2 simbols');
+        console.log("carielia")
+        setSucessname = false;
+	}     
+
+
+    else {
+        console.log("araa carieli")
+        setSuccessFor(username);
+		setSucessname = true;
 	}
 
 
     //email validation
     if(emailVal === '') {
 		setErrorFor(email,'Invalid email' ,'email cannot be blank');
-	} else if (emailReg == false ){
-        setErrorFor(email, 'Invalid email' ,'Please enter valid email address');
-    }
-    
+        setSucessemail = false;
+	}     
     else {
-		setSuccessFor(email);
+        setSuccessFor(email);
+        setSucessemail = true;
 	}
 
 
@@ -95,27 +243,26 @@ function checkInputs(){
     //phone number validation 
     if(phoneVal === '') {
 		setErrorFor(phone, 'Invalid phone' ,'Number cannot be blank');
- 
-	} else if (phoneReg == false ){
-        setErrorFor(phone, 'Invalid phone' ,'Please enter valid phone number ');
-    }
-    
+        setSucessphone = false;
+	}
     else {
-		setSuccessFor(phone);
+        setSuccessFor(phone);
+        setSucessphone = true;
 	}
 
 
     //date validation
     if(dateVal === '') {
         setErrorFor(date, ' Invalid date\r\n' ,'date cannot be blank');
-
-    } else if (dateReg == false ){
-        setErrorFor(date, 'Invalid date' ,'Please enter valid date ');
+        setSucessdate = false;
     }
 
     else {
         setSuccessFor(date);
+        setSucessdate = true;
     }
+    console.log("checked on submit");
+}
 
 
 
@@ -123,10 +270,7 @@ function checkInputs(){
     function setErrorFor(input, type, solve) {
         const formControl = input.parentElement;
         formControl.className = 'form_control';
-        
 
-    
-        
         const errorType = document.getElementById('error_exp');
         const errorSolve = document.getElementById('error_solve');
         input.className = 'error';
@@ -135,6 +279,7 @@ function checkInputs(){
         errorType.innerText = type;
         errorSolve.innerText = solve;
         
+        
     }
     
     function setSuccessFor(input) {
@@ -142,24 +287,8 @@ function checkInputs(){
         formControl.className = 'form_control success';
         input.className = '';
         errorWindow.className = 'none';
-        called += 1;
+        setSucess = input;
         
     }
 
-
-    // error window and navigation style
-    if (called  < 5) {
-        errorWindow.className = 'error_window';
-    } else {
-        document.getElementById('personal_anc').innerText = ' ';
-        document.getElementById('personal_anc').className = 'nav_anc anc_bg';
-        window.location.replace("/experience.html");
-        console.log("mushaobs")
-    }
-
-
-
-
-}
-
-
+  
